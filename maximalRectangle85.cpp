b3ef99c736c4b6480820c6766fc8743206f76561
Mohit Sengar <9759063029ms@gmail.com>
2026-03-25 10:50:41 +0530

    leetcode 84 and 85 solved

---
+++ maximalRectangle85.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+
+        // nearest smaller to left
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+
+            if(st.empty()) left[i] = -1;
+            else left[i] = st.top();
+
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            st.pop();
+        }
+
+        // nearest smaller to right
+        for(int i = n-1 ; i >= 0; i--){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+            
+            // right boundary in n
+            if(st.empty()) right[i] = n;
+            else right[i] = st.top();
+            st.push(i);
+        }
+
+        // while(!st.empty()){
+        //     st.pop();
+        // }   
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            int w = right[i] - left[i] - 1;
+            int h = heights[i];
+            ans = max(ans, w*h);
+        }
+
+        return ans;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int area = 0;
+        vector<int> h(matrix[0].begin(), matrix[0].end());
+        for(int i = 0; i < m; i++){
+            area = max(area, largestRectangleArea(h));
+            for(int j = 0; j < n; j++){
+                h[j] += matrix[i][j];
+            }
+        }
+        return area;
+    }
+};
